8a8164e34f436a982d59f333cb2a01256263705e
Gabriel M. Gorospe <gabemg525@gmail.com>
2025-02-19 17:11:24 -0800

    ok

--- VisitorCount.cs
+++ VisitorCount.cs
@@ -2,10 +2,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Azure;
-using Azure.Core;
-using Azure.Data.Tables;
-using Azure.Identity;
 
 namespace Visitor.Function
 {
@@ -22,17 +18,6 @@ namespace Visitor.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            // Authenticate using DefaultAzureCredential
-            TokenCredential credential = new DefaultAzureCredential();
-            string tableEndpoint = "https://cosmos-db-table-pbekhm5evwdtc.table.cosmos.azure.com:443/";
-
-            // Initialize TableServiceClient
-            TableServiceClient serviceClient = new TableServiceClient(new Uri(tableEndpoint), credential);
-
-            // Get a reference to your table
-            TableClient client = serviceClient.GetTableClient("ProductsTable");
-
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
